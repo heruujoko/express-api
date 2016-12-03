7f08fabb98546730d3b3598e7e4e33dd7e95b8aa
Heru Joko <herujokoutomo@gmail.com>
2016-12-03 22:17:27 +0800

    authentication with passport

--- src/index.js
+++ src/index.js
@@ -3,19 +3,52 @@ import http from 'http'
 import fs from 'fs'
 import jwt from 'jsonwebtoken'
 import bodyParser from 'body-parser'
+import passport from 'passport'
+import morgan from 'morgan'
+
+let LocalStrategy = require('passport-local').Strategy;
 
 let app = express()
 http.createServer(app)
-app.models = require('./models')(app);
+app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
-
+app.use(bodyParser.json()) // parse application/json
 app.jwt = jwt
+app.models = require('./models')(app);
+
+// passport strategy
+app.use(passport.initialize())
+passport.use('local',new LocalStrategy({
+        usernameField:"email",
+        passwordField:"password"
+    },
+    function(email, password, done){
+        console.log('email '+email);
+        app.models.User.findOne({
+            where: {
+                email: email
+            }
+        }).then((user) => {
+            if(!user){
+                return done(null, false, { message: 'Incorrect username.' })
+            } else {
+                done(null,user)
+            }
+        })
+    }
+))
+
+passport.serializeUser(function(user, done) {
+  done(null, "shafdahgsdhsagdh");
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null,{ username: "heru"})
+});
 app.middlewares = require('./middlewares')(app,fs)
 app.controllers = require('./controllers')(app,fs)
 
-const routes = require('./routes')(app)
+const routes = require('./routes')(app,passport)
 
 app.listen(3000)
 console.log('Application started.');
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,18 @@
  *  Setup routes
  */
 
-module.exports = (app) => {
+module.exports = (app,passport) => {
+    app.post('/login',
+      passport.authenticate('local'),
+      function(req, res) {
+        // If this function gets called, authentication was successful.
+        // `req.user` contains the authenticated user.
+        let token = app.jwt.sign({ email: req.user.email },'f9ICFKBHBmlN3Vs4MF27')
+        res.json({
+            status: 'success',
+            token: token
+        })
+    });
     app.get('/',app.controllers.SpecController.index);
     app.get('/users',app.middlewares.JWTMiddleware.index,app.controllers.UsersController.index);
     app.get('/token',(req,res) => {
@@ -17,8 +28,5 @@ module.exports = (app) => {
             status: "success",
             payload: req.jwtpayload
         })
-    })
-    app.post('/',(req,res) => {
-        res.json(req.body)
     })
 }
